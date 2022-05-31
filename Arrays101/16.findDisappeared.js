dd796000bdf9700186c95d4b66dd184d6c8fe321
rishi1717 <rshrrrss@gmail.com>
2022-05-31 19:37:49 +0530

    find disappeared

--- Arrays101/16.findDisappeared.js
+++ Arrays101/16.findDisappeared.js
@@ -1,3 +1,15 @@
 var findDisappearedNumbers = function (nums) {
-    
+	let len = nums.length
+    let set = new Set(nums)
+	let result = []
+
+	for (let i = 1; i <= len; i++) {
+		if (set.has(i)) {
+			continue
+		}
+		result.push(i)
+	}
+	return result
 }
+
+console.log(findDisappearedNumbers([4, 3, 2, 7, 8, 2, 3, 1]))
